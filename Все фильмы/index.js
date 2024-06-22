03df0fdfb4c414d8069d06dcbf15a6bfb36415ab
rehdfhbv <nazarenkoilya2006@gmail.com>
2024-06-22 17:32:57 +0200

    start

---
+++ Все фильмы/index.js
@@ -0,0 +1,7 @@
+// let close1 = document.getElementById("burgerClose")
+// burgerOpen.addEventListener("click", () => {
+//     burgerMenu.style.right = "-20%"
+// })
+// close1.addEventListener("click", () => {
+//     burgerMenu.style.right = "-40%"
+// })
